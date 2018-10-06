cf7238291cb4f9bf28d431345b82cac614a6aae8
Orta <orta.therox+github@gmail.com>
2018-10-06 10:54:34 -0400

    Create Dangerfile.swift

---
+++ danger-swift/Dangerfile.swift
@@ -0,0 +1,8 @@
+// Ref: https://github.com/Moya/Harvey/blob/master/Dangerfile.swift
+
+import Foundation
+import Danger
+import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
+
+let danger = Danger()
+warn("Danger Swift is working")
