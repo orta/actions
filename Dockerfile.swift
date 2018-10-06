7f229d61c2c3551466ebf3edaa8173ab18f1a6b6
Orta Therox <orta.therox@gmail.com>
2018-10-06 11:12:39 -0400

    Add support for Danger Swift

---
+++ Dockerfile.swift
@@ -0,0 +1,8 @@
+// https://github.com/Moya/Harvey/blob/master/Dangerfile.swift
+
+import Foundation
+import Danger
+import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
+
+let danger = Danger()
+message("Hello, from danger swift")
