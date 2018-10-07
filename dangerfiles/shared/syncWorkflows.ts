11eb4899998d88bb1acae2c9f0f8f07e25b680c6
Orta Therox <orta.therox@gmail.com>
2018-10-07 12:47:22 -0400

    Adds dangerfiles

---
+++ dangerfiles/shared/syncWorkflows.ts
@@ -0,0 +1,49 @@
+import {danger} from "danger"
+import {Push} from "github-webhook-event-types"
+
+// Still undecided on how args like "core repos" should work
+// probably env vars
+
+export default async (push:Push) => {
+  // Bail early on non-master merges
+  if (push.ref !== "master") {
+    return 
+  }
+  
+  const thisRepo = push.repository.full_name
+  const coreRepos = ["orta/actions2"]
+
+  const workflowPath = ".github/main.workflow"
+  const currentWorkflowContent =  await danger.github.utils.fileContents(workflowPath, thisRepo, push.ref)
+
+  for (const fullRepo of coreRepos) {
+    const owner = fullRepo.split("/")[0]
+    const repo = fullRepo.split("/")[1]
+
+    // Grab the repo's workflow
+    const repoWorkflowContent =  await danger.github.utils.fileContents(workflowPath, fullRepo)
+    if (currentWorkflowContent === repoWorkflowContent) {
+      continue
+    }
+
+    // Make set of FS changes as an object
+    const newFileMap: any = {}
+    newFileMap[workflowPath] = currentWorkflowContent
+
+    // Creates a new branch called `update_workflow`, from `master`. Creates a commit with
+    // the changes above and the message "Sets up ...". Then sends a PR to `orta/actions`
+    // with the title "Update to the GitHub ..." and the body "The main workflow file ...".
+    await danger.github.utils.createOrUpdatePR(
+      {
+        title: "Update to the GitHub Workflow frile",
+        body: `The main workflow file in ${thisRepo} was updated, this auot-generated PR moves the changes over.`,
+        owner,
+        repo,
+        baseBranch: "master",
+        newBranchName: "update_workflow",
+        commitMessage: `Migrates the changes from ${thisRepo}`,
+      },
+      newFileMap
+    )
+  }
+}
